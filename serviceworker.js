8342820d790d960fbdd2bc321d8127bfd0798fd8
dejonghe <dejonghe@users.noreply.github.com>
2025-03-16 17:44:46 +0000

    deploy: ede561111beab4353c44f13263543d27d6b75e41

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'cache-2025-03-16T06:26:40.478Z'
+const CACHE_NAME = 'cache-2025-03-16T17:44:43.494Z'
 const OFFLINE_PAGE_URL = '/offline/'
 const PRECACHE_RESOURCES = []
 const IGNORED_HOSTS = ['localhost']
