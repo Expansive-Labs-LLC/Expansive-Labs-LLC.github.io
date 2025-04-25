aaa386d88db312f522992e4fd10472a8281c984c
Mother7Goose <Mother7Goose@users.noreply.github.com>
2025-04-25 20:54:31 +0000

    deploy: 534fad18d4c61a807ce84a2b02a2850ddfbd1d8a

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'cache-2025-04-18T20:10:47.549Z'
+const CACHE_NAME = 'cache-2025-04-25T20:54:27.873Z'
 const OFFLINE_PAGE_URL = '/offline/'
 const PRECACHE_RESOURCES = []
 const IGNORED_HOSTS = ['localhost']
