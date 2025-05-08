2f74915a5a89de6320c5b8f53b0e6d7dc67ab9e8
JP0121 <JP0121@users.noreply.github.com>
2025-05-08 20:55:48 +0000

    deploy: 81ea2fea6f0c40c30fd3bd49918f133d1f2333b9

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'cache-2025-05-08T20:50:32.168Z'
+const CACHE_NAME = 'cache-2025-05-08T20:55:45.527Z'
 const OFFLINE_PAGE_URL = '/offline/'
 const PRECACHE_RESOURCES = []
 const IGNORED_HOSTS = ['localhost']
